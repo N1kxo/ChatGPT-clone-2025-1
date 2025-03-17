c1025b3c525eba2f2b4f8e1ddd5575fef20fbaff
N1kxo <nico9ms@hotmail.com>
2025-03-17 13:45:54 -0500

    update splashscreen

--- app/splashscreen.tsx
+++ app/splashscreen.tsx
@@ -1,12 +1,15 @@
 import { View, Text, Button, TouchableOpacity, StyleSheet, ScrollView, Image } from "react-native";
 import React from "react";
 import { styles } from "./styles";
-import { useNavigation } from "expo-router";
+import { useRouter } from "expo-router";
 
 export default function SplashScreen() {
+    const router = useRouter();
     return (
         <View style={styles.backGround}>
+        <TouchableOpacity onPress={() => router.push("/welcome")}>
         <Image source={require('../assets/images/ChatGPT-logo.png')} />
+        </TouchableOpacity>
 </View>
 
     )
